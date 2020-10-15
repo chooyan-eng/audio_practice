d3a897d26f01ee0cd6afb9a126e0c3d73b9e3305
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2020-10-16 01:47:32 +0900

    first commit

---
+++ ios/Pods/Target Support Files/flutter_sound/flutter_sound-umbrella.h
@@ -0,0 +1,26 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Flauto.h"
+#import "FlautoManager.h"
+#import "FlautoPlayerManager.h"
+#import "FlautoRecorderManager.h"
+#import "FlutterFFmpegPlugin.h"
+#import "FlutterSoundPlayer.h"
+#import "FlutterSoundRecorder.h"
+#import "PlayerEngine.h"
+#import "Track.h"
+#import "TrackPlayer.h"
+
+FOUNDATION_EXPORT double flutter_soundVersionNumber;
+FOUNDATION_EXPORT const unsigned char flutter_soundVersionString[];
+
